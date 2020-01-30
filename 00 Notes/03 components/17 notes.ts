8afbbf7484301c796fc041825cfd317d5e68212d
humbertojacobm <humbertojacobm@gmail.com>
2020-01-30 10:35:14 -0500

    video 17 using angular pipes

---
+++ 00 Notes/03 components/17 notes.ts
@@ -0,0 +1,14 @@
+//los generadores de angular, nos ayudan a ser mas productivos.
+
+// angular.io/cli/generate
+
+// ng g c cart
+
+//esto esta agregando el componente en el modulo, y crear la clase componente
+// crea el template, el estilo y el spec.
+
+//usa linters para obtener la mejor sugerencia de codigo.
+
+//ng lint
+//   esto va a revisar que no tengas errores de escritura de codigo en tu proyecto
+
