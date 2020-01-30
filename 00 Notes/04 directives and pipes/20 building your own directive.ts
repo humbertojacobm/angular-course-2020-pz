1866d7af9daf9ee6d6850899a65d0618d61e68f6
humbertojacobm <humbertojacobm@gmail.com>
2020-01-30 10:55:57 -0500

    video 20, building your own directive

---
+++ 00 Notes/04 directives and pipes/20 building your own directive.ts
@@ -0,0 +1,3 @@
+//directivas: son importantes para modificar el dom de un elemento especifico.
+//   estan hechas para modificar las propiedades del dom dinamicamente.
+// se podria usar en un feature de autocompletar.
